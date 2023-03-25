1500dc51cf5e306287134f136c9ff869b9d531da
EC2 Default User <yasuzyun@yahoo.co.jp>
2023-03-25 07:14:17 +0000

    Initial Commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
   
   config.hosts << "64f20233e31944e58831c2331844b890.vfs.cloud9.ap-northeast-1.amazonaws.com"
   
+  config.active_job.queue_adapter = :inline
 end
